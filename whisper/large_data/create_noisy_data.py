6f9060450a233c9b288a54eee7f158a982038f0c
zahra rahmani <82266839+zahra-rahmani@users.noreply.github.com>
2025-12-30 13:48:52 +0330

    minor changes made

--- whisper/large_data/create_noisy_data.py
+++ whisper/large_data/create_noisy_data.py
@@ -7,7 +7,7 @@ from glob import glob
 # Augmentation pipeline
 augment = Compose([
     AddBackgroundNoise(
-        sounds_path="/home/zahra/hypo-light/generate_data/noise_dataset",
+        sounds_path="path_to_musan_noise",
         min_snr_db=5,
         max_snr_db=15,
         p=0.95
@@ -16,21 +16,18 @@ augment = Compose([
 ])
 
 # Load speech WAV files
-speech_files = sorted(glob("/home/zahra/hypo-light/generate_data/test_data/wavs/*.wav"))
+speech_files = sorted(glob("path_to_wav_files"))
 
 # Output folder
-output_dir = "noisy_test_data"
+output_dir = "output_path"
 os.makedirs(output_dir, exist_ok=True)
 
-# Loop over each speech file and apply augmentation
 for i, wav_path in enumerate(speech_files):
-    # Load audio
     audio, sr = sf.read(wav_path)
 
     # Apply augmentation
     noisy_audio = augment(samples=audio, sample_rate=sr)
 
-    # Save augmented audio
     output_path = os.path.join(output_dir, f"noisy_test_{i}.wav")
     sf.write(output_path, noisy_audio, samplerate=sr)
 
